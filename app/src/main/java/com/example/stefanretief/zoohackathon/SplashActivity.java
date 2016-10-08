1e0fbe69079b84f9bfce59c7a4de6720722b6e25
arangoaurelio <arango.l.aurelio@gmail.com>
2016-10-08 15:44:55 -0700

    App-runs

--- app/src/main/java/com/example/stefanretief/zoohackathon/SplashActivity.java
+++ app/src/main/java/com/example/stefanretief/zoohackathon/SplashActivity.java
@@ -3,13 +3,14 @@ package com.example.stefanretief.zoohackathon;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Aurelio Arango on 10/8/2016.
  *
  */
 
-public class SplashActivity  extends Activity{
+public class SplashActivity  extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
