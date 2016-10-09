8572e47adc83372ddf7317abdc70880fd53f1d60
arangoaurelio <arango.l.aurelio@gmail.com>
2016-10-08 17:29:29 -0700

    Notification class

---
+++ app/src/main/java/com/example/stefanretief/zoohackathon/NotificationActivity.java
@@ -0,0 +1,18 @@
+package com.example.stefanretief.zoohackathon;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by Aurelio Arango on 10/8/2016.
+ *
+ */
+
+public class NotificationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
