c3a4392a064d640415d6fa81d28eef12553c5793
Stefan Retief <stefan.retief@me.com>
2016-10-08 18:30:17 -0700

    Main button works

--- app/src/main/java/com/example/stefanretief/zoohackathon/MainActivity.java
+++ app/src/main/java/com/example/stefanretief/zoohackathon/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.text.TextWatcher;
 
 public class MainActivity extends Activity {
 
@@ -18,12 +19,12 @@ public class MainActivity extends Activity {
         final EditText from = (EditText) findViewById(R.id.fromLoc);
         final EditText to = (EditText) findViewById(R.id.toLoc);
 
-
         final Button startVoyage = (Button) findViewById(R.id.Voyage);
         startVoyage.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (from.getText() != null && to.getText() != null) {
+
+                    if (from.getText() != "From" && to.getText() != "To") {
                         Intent goToVoyage = new Intent(MainActivity.this, InitialVoyage.class);
                         goToVoyage.putExtra("fromVal", from.getText());
                         goToVoyage.putExtra("toVal", to.getText());
